591c738fe3bed3d1c4d203baec3d8022c7b7f9d1
Adrien Castagliola <adrien.castagliola@edflex.com>
2022-03-20 17:21:14 +0100

    refactor: add mime type to link

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,6 +6,8 @@ import Header from "../header/header";
 import { ToastProvider } from "../../contexts/ToastContext/ToastContext";
 
 import style from "./App.style.css";
+// import "../../styles/reset.css";
+// import "../../styles/style.css";
 
 const App: FunctionalComponent = () => {
   return (
